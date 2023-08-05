9b00b5c0bb55edf5e2d4065c52c08ddddd507979
Naveen <naveenkumarm572@gmail.com>
2023-08-05 16:26:20 +0530

    build a responsive layout

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import { Button, ButtonGroup } from '@chakra-ui/react'
-import './App.css'
+import { useState } from "react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <Button colorScheme='blue'>Button</Button>
-    </div>
-  )
+    <>
+      <Grid gridTemplateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`
+      }}>
+        <GridItem area='nav' bg="coral">Nav</GridItem>
+        <Show above="lg">
+          <GridItem area='aside'  bg="gold" >Aside</GridItem>
+        </Show>
+        <GridItem area='main' bg="dodgerblue" >Main</GridItem>
+      </Grid>
+    </>
+  );
 }
 
-export default App
+export default App;
